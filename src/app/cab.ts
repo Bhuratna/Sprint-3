4f87ed52d0672ea19f1a5acf071e15f493a95483
Bhuratna Mishra <Bhuratanm093@gmail.com>
2020-11-28 11:59:18 +0530

    updated code

--- src/app/cab.ts
+++
@@ -1,9 +0,0 @@
-export class Cab {
-    carType:string;
-    perKmRate:string;
-    constructor(carType:string,perKmRate:string)
-    {
-        this.carType=carType;
-        this.perKmRate=perKmRate;
-    }
-}
